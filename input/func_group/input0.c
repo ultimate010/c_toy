1ec247bd6db229ebe8f55e4f2d1c71bdf2ab436d
ultimate010 <ultimate010@gmail.com>
2013-06-13 08:53:52 +0800

    添加函数调用答案

---
+++ input/func_group/input0.c
@@ -0,0 +1,42 @@
+/*
+ * 需要实现功能跳过注释，输出函数调用关系，并打输出简单打印语句
+ * Zaks Wang
+ * 2013-5-18
+ */
+//comment
+/*
+ * 示例输出为：
+ *  In function fun2,print string is :
+ *  I am fun2\n
+ *  In function fun3,print string is :
+ *  I am fun3\n
+ *  Function Call hierarchy
+ *  main->fun0->fun1->fun3->End
+ */
+
+void fun0();
+int fun1();
+double fun2();
+float fun3();
+short main();
+
+void main(){
+  fun0();
+}
+
+void fun0(){
+  fun1();
+}
+
+void fun1(){
+  fun2()；
+}
+
+void fun2(){
+  printf("I am fun2\n");
+}
+
+void fun3(){
+  printf("I am fun3\n");
+  fun2();
+}
